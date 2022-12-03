c60a40352fecd5c0610f8c64ca03879fc19209b6
drew-k <drewkinney11@yahoo.com>
2022-12-02 21:47:01 -0500

    Add console formatting class.

---
+++ format.py
@@ -0,0 +1,10 @@
+class Format():
+    """ Console output format options """
+    red = '\033[91m'
+    green = '\033[92m'
+    yellow = '\033[93m'
+    blue = '\033[94m'
+    bold = '\033[1m'
+    italics = '\033[3m'
+    underline = '\033[4m'
+    reset = '\033[0m'
